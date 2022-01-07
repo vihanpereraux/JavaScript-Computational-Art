f8dc0b06d6fa18fca14e4693a0590c4920254901
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2022-01-07 19:30:52 +0530

    Initializing funcs

---
+++ sketch.js
@@ -0,0 +1,14 @@
+// Loads everything to the program
+let img;
+function prelaod(){
+  img = loadImage('assets/model.jpg');
+}
+
+function setup() {
+  createCanvas(1280, 1706);
+}
+
+// Loops forever
+function draw() {
+  background(220);
+}
