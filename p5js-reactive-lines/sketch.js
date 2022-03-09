046013cac0cf682d6e930c01ada2a29305533c73
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2022-03-09 10:14:03 +0530

    Gradient logics added

--- p5js-reactive-lines/sketch.js
+++ p5js-reactive-lines/sketch.js
@@ -9,8 +9,12 @@ function setup()
 }
 
 var counter = 0;
+var counterm = 0;
+
 function draw() 
 {   
+  counter = counter + 1 ;
+
   // creates an empty object
   var col = {};
   var r = col;
@@ -28,21 +32,40 @@ function draw()
   //stroke(68, 214, 44);
   
   translate(width / 2, height / 2);
-  
   rotate( random(20, 50) );
-  //rotate( 60 );
   rect(0, 0, 10*random(10,20), 1);
 
+  // create gradient
   var canvas = document.getElementById('box');
   var c = canvas.getContext('2d');
 
-  // Create Gradient
-  //var grad = c.createLinearGradient(0,0,100,0);
-  var grad = c.createLinearGradient(0,0,random(10, 150),random(100, 150));
-  grad.addColorStop(0, "white");
-  grad.addColorStop(1, "lightblue");
-  c.fillStyle = grad;
-  c.fillRect(0, 0, 500, 500);
+  // gradient loop
+  if (counter > 600) 
+  { 
+    // <- right | bottom - top | left -> | top - bottom 
+    var grad = c.createLinearGradient(500-counter/5,0,0,0);
+    console.log(counter);
+    //var grad = c.createLinearGradient(0,0,10+counter/2,0);
+    
+    // adding to colors 
+    grad.addColorStop(0, "red");
+    grad.addColorStop(1, "lightblue");
+    c.fillStyle = grad;
+    c.fillRect(0, 0, windowWidth, 500);
+    if ( counter == 1200){ counter = 0 }
+  }
+  else if (counter < 600)
+  {
+    // <- right | bottom - top | left -> | top - bottom 
+    //var grad = c.createLinearGradient(0,0,0,100);
+    var grad = c.createLinearGradient(0,0,80+counter/2,0);
+    
+    // adding to colors 
+    grad.addColorStop(0, "red");
+    grad.addColorStop(1, "lightblue");
+    c.fillStyle = grad;
+    c.fillRect(0, 0, windowWidth, 500);
+  }
 }
 
 
